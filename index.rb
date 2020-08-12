ef8bf2772867991020ce413b9d65c0a25e14bac3
sbrook13 <sbrook13@gmail.com>
2020-08-12 14:40:01 -0600

    solution 8.12.2020

--- index.rb
+++ index.rb
@@ -1,3 +1,26 @@
+#8.12.2020 Solution "Western Suburbs Croquet Club" 
+
+# Senior, at least 55 years old and handicap greater than 7. 
+
+# Best Practices / Clever
+def openOrSenior(data)
+  data.map { |age, handicap| age >= 55 && handicap > 7 ? "Senior" : "Open" }
+end
+
+# original answer
+
+def openOrSenior(data)
+  players = []
+  data.each do |player|
+    if player[0] >= 55 && player[1] > 7
+      players << "Senior"
+    else
+      players << "Open"
+    end
+  end
+  players
+end
+
 # 8.11.2020 Solution "fill name in here"
 
 def numberToString(num)
