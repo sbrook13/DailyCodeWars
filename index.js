e3f0d62fe80d28692cde651b54a96c132febe2d8
sbrook13 <sbrook13@gmail.com>
2020-11-15 21:27:32 -0700

    codewars for 11.15.2020

--- index.js
+++ index.js
@@ -33,3 +33,12 @@ function compOther(a, b) {
 a1 = [121, 144, 19, 161, 19, 144, 19, 11];
 a2 = [11*11, 121*121, 144*144, 19*19, 161*161, 19*19, 144*144, 19*19];
 comp(a1, a2)
+
+// 11.15.2020 finding pairs 
+
+function validBraces(braces){
+  while(braces.indexOf("{}") != -1 || braces.indexOf("()") != -1 || braces.indexOf("[]") != -1){
+    braces = braces.replace("{}", "").replace("()", "").replace("[]", "");
+  }
+  return braces.length == 0;
+}
